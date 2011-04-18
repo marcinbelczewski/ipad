16209bd6c537484b4ae664741debe696f56de78b
Worker <worker@Workers-MacBook-Pro.local>
2011-04-18 15:00:25 +0200

    commit again

--- Quotes/Quotes/QuotesAppDelegate.h
+++ Quotes/Quotes/QuotesAppDelegate.h
@@ -13,7 +13,7 @@
 @interface QuotesAppDelegate : NSObject <UIApplicationDelegate> {
 
 }
-//something modified
+//something modified again
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
 @property (nonatomic, retain) IBOutlet QuotesViewController *viewController;
